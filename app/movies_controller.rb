7cc5896b480ad8d5714a49d5904a02e05d3bb66a
Daniel Ran <danielr1735@gmail.com>
2018-07-09 18:25:56 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -8,7 +8,6 @@
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  movie.title = "This is a title."
   movie.release_date = Time.now
   movie.director = "This is a director."
   movie.lead = "This is a lead."
